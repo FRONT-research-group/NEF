fc38a241b6b7f8fb0d28ddb06d84188f9f2db73d
Panagiotis Pavlidis <p.pavlidis@iit.demokritos.gr>
2025-10-03 15:28:59 +0300

    Enhance NEF to provide logs during auth and retry mechanism for nbf

--- MonitoringEventAPI/app/auth.py
+++ MonitoringEventAPI/app/auth.py
@@ -1,9 +1,12 @@
+import time
 from jose import JWTError, jwt, ExpiredSignatureError
 from OpenSSL import crypto
 from fastapi import HTTPException, status, Depends
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from app.config import get_settings
+from app.utils.logger import get_app_logger
 
+logger = get_app_logger()
 settings = get_settings()
 
 PUB_KEY_PATH = settings.PUB_KEY_PATH
@@ -24,17 +27,29 @@ PUB_KEY = _get_public_key(PUB_KEY_PATH)
 
 def verify_token(credentials: HTTPAuthorizationCredentials = Depends(security)) -> None:
     token = credentials.credentials
-    try:
-        jwt.decode(token, PUB_KEY, algorithms=[ALGORITHM])
-    except ExpiredSignatureError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token has expired",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    except JWTError:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
+    logger.info("token is: %s", token)
+    retries = 0
+    while (retries < 3):
+        try:
+            jwt.decode(token, PUB_KEY, algorithms=[ALGORITHM])
+            logger.info("Authorization was successful")
+            return
+        except ExpiredSignatureError as exc:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token has expired",
+                headers={"WWW-Authenticate": "Bearer"},
+            ) from exc
+        except JWTError as exc:
+            logger.error("JWT Verification Error: %s", exc)
+            if "nbf" in str(exc):
+                logger.error("Token not valid yet")
+                logger.info("Waiting for token to become valid...")
+                time.sleep(0.5)
+                retries += 1
+            else:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Invalid token",
+                    headers={"WWW-Authenticate": "Bearer"},
+                ) from exc
