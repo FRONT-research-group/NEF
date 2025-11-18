2a07e2d9baa13ec662e623d9885d268364a50417
Panagiotis Pavlidis <p.pavlidis@iit.demokritos.gr>
2025-11-18 15:24:34 +0200

    Implement cellId to area mapping

--- MonitoringEventAPI/app/main.py
+++ MonitoringEventAPI/app/main.py
@@ -26,9 +26,9 @@ logger.info(f"CAMARA CASE: {settings.camara_case}")
 logger.info(
     f"Map MSISDN to IMSI Collection Name: {settings.map_msisdn_imsi_collection_name}"
 )
-# logger.info(
-#     f"Map Cell ID to Polygon Collection Name: {settings.map_cellId_to_polygon_collection_name}"
-# )
+logger.info(
+    f"Map Cell ID to Polygon Collection Name: {settings.map_cellId_to_polygon_collection_name}"
+)
 logger.info(f"Auth Enabled: {settings.auth_enabled}")
 if settings.auth_enabled:
     logger.info(f"Public Key Path: {settings.pub_key_path}")
